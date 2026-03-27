e5617278c125e8f5f2b8f176ea8abc108e9df513
Mugia N M <seemugia1@gmail.com>
2026-03-27 22:33:20 +0700

    fix:: prisma query logging
    Signed-off-by: Mugia N M <seemugia1@gmail.com>

--- lib/db.ts
+++ lib/db.ts
@@ -9,7 +9,10 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  adapter,
+  log: ['query', 'info', 'warn', 'error'],
+})
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db
