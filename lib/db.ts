472a10eb7053a70682c624d88397e1c855e004b2
Mugia N M <seemugia1@gmail.com>
2026-03-27 22:38:54 +0700

    feat:: add query time taken on log
    Signed-off-by: Mugia N M <seemugia1@gmail.com>

--- lib/db.ts
+++ lib/db.ts
@@ -9,10 +9,25 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-export const db = globalForPrisma.prisma ?? new PrismaClient({
-  adapter,
-  log: ['query', 'info', 'warn', 'error'],
-})
+const getPrisma = () => {
+  const client = new PrismaClient({
+    adapter,
+    log: [
+      { emit: 'event', level: 'query' },
+      { emit: 'stdout', level: 'error' },
+      { emit: 'stdout', level: 'warn' },
+    ],
+  })
+
+  client.$on('query', (e: any) => {
+    console.log(`[Prisma] query executed in ${e.duration}ms`)
+    console.log(`Query: ${e.query}`)
+  })
+
+  return client
+}
+
+export const db = globalForPrisma.prisma ?? getPrisma()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db
