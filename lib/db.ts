18975e01ae4fe2f28d8315221ec4fa91e3d72768
Mugia N M <seemugia1@gmail.com>
2026-04-05 23:09:39 +0700

    fix: import prisma libsql
    Signed-off-by: Mugia N M <seemugia1@gmail.com>

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { createClient } from '@libsql/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -11,7 +11,7 @@ const libsql = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN,
 })
 
-const adapter = new PrismaLibSQL(libsql)
+const adapter = new PrismaLibSql(libsql)
 
 const getPrisma = () => {
   const client = new PrismaClient({
